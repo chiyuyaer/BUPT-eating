0829d802c55ef7a12df71ed8588111150dd7e471
chiyuyaer <75930831+chiyuyaer@users.noreply.github.com>
2020-12-13 21:20:51 +0800

    Add files via upload

---
+++ 食之邮道/pages/category/category.js
@@ -0,0 +1,58 @@
+// pages/category/category.js
+Page({
+    /**
+     * 页面的初始数据
+     */
+    
+     go: function(){
+      wx.navigateTo({
+        url: 'pages/list1/list1',
+      })
+    },
+     go2: function(){
+      wx.navigateTo({
+        url: 'pages/list2/list2',
+      })
+    },
+     go3: function(){
+      wx.navigateTo({
+        url: 'pages/list3/list3',
+      })
+    },
+     go4: function(){
+      wx.navigateTo({
+        url: 'pages/list4/list4',
+      })
+    },
+    go5: function(){
+      wx.navigateTo({
+        url: 'pages/list5/list5',
+      })
+    },
+    go6: function(){
+      wx.navigateTo({
+        url: 'pages/list6/list6',
+      })
+    },
+    go7: function(){
+      wx.navigateTo({
+        url: 'pages/list7/list7',
+      })
+    },
+    go8: function(){
+      wx.navigateTo({
+        url: 'pages/list8/list8',
+      })
+    },
+    go9: function(){
+      wx.navigateTo({
+        url: 'pages/list9/list9',
+      })
+    },
+    go10: function(){
+      wx.navigateTo({
+        url: 'pages/list10/list10',
+      })
+    }
+    
+  })
